65d77d20568725152eabf57208f872f212f68c59
Aaron Samuel Mathew <103445014+AaronSMathew@users.noreply.github.com>
2024-09-07 09:35:06 +0530

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,21 +13,13 @@ if($action == 'login_faculty'){
 	if($login_faculty)
 		echo $login_faculty;
 }
-if($action == 'login2'){
-	$login = $crud->login2();
-	if($login)
-		echo $login;
-}
+ 
 if($action == 'logout'){
 	$logout = $crud->logout();
 	if($logout)
 		echo $logout;
 }
-if($action == 'logout2'){
-	$logout = $crud->logout2();
-	if($logout)
-		echo $logout;
-}
+ 
 if($action == 'save_user'){
 	$save = $crud->save_user();
 	if($save)
@@ -48,11 +40,7 @@ if($action == 'update_account'){
 	if($save)
 		echo $save;
 }
-if($action == "save_settings"){
-	$save = $crud->save_settings();
-	if($save)
-		echo $save;
-}
+ 
 if($action == "save_course"){
 	$save = $crud->save_course();
 	if($save)
@@ -119,40 +107,6 @@ if($action == "delete_comment"){
 
 }
 
-if($action == "save_event"){
-	$save = $crud->save_event();
-	if($save)
-		echo $save;
-}
-if($action == "delete_event"){
-	$save = $crud->delete_event();
-	if($save)
-		echo $save;
-}	
-if($action == "participate"){
-	$save = $crud->participate();
-	if($save)
-		echo $save;
-}
-if($action == "get_venue_report"){
-	$get = $crud->get_venue_report();
-	if($get)
-		echo $get;
-}
-if($action == "save_art_fs"){
-	$save = $crud->save_art_fs();
-	if($save)
-		echo $save;
-}
-if($action == "delete_art_fs"){
-	$save = $crud->delete_art_fs();
-	if($save)
-		echo $save;
-}
-if($action == "get_pdetails"){
-	$get = $crud->get_pdetails();
-	if($get)
-		echo $get;
-}
+ 
 ob_end_flush();
 ?>
